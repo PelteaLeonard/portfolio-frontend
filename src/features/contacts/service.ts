6609e8c2c92a4b643d6831915fc2265e1599fff6
Leonard Peltea <leonard.peltea@yahoo.com>
2024-05-19 10:27:34 +0100

    Edit some functionalities

--- src/features/contacts/service.ts
+++ src/features/contacts/service.ts
@@ -1,7 +1,10 @@
 import RequestUtils from "../../utils/request-utils";
 import { CreateRequestPayload } from "./types";
 
-const CONTACTS_BASE_URL = `/api/contacts`;
+const BACKEND_API =
+  "https://leonard-portfolio-backend-d1a47d594063.herokuapp.com";
+
+const CONTACTS_BASE_URL = `${BACKEND_API}/api/contacts`;
 
 const create = async (requestPayload: CreateRequestPayload) => {
   return RequestUtils.post(CONTACTS_BASE_URL, requestPayload);
